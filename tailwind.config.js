02a1ad82d793947cd129b3c5e8e39ebd5b33e080
cherry-nft <109643299+cherry-nft@users.noreply.github.com>
2024-04-24 16:56:42 +0000

    shadcn2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  presets: [require('./src/subframe/tailwind.config.js'), require("./components/subframe/subframe/tailwind.config.js")],
+  presets: [require("./components/subframe/subframe/tailwind.config.js")],
   content: ['./pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    "./src/subframe/**/*.{js,ts,jsx,tsx}", "./components/subframe/subframe/**/*.{tsx,ts,js,jsx}"],
+    "./components/subframe/subframe/**/*.{tsx,ts,js,jsx}"],
   theme: {
     extend: {
       colors: {},
